c3b08f0e060e473a11c5540c7bb4bbca46bc427a
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-12 20:31:33 +0530

    Use Naming Convention

--- CartesianProgram.java
+++ CartesianProgram.java
@@ -2,43 +2,35 @@ package com.blbz;
 import java.util.*;
 public class CartesianProgram {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 int x1,y1,x2,y2;
-	        Scanner sc = new Scanner(System.in);
-	        x1 = sc.nextInt();
-	        y1 = sc.nextInt();
-	        x2 = sc.nextInt();
-	        y2 = sc.nextInt();
+	public static int lengthOfLine()
+    {
+        	int x1,y1,x2,y2;
+        	Scanner sc = new Scanner(System.in);
+        	x1 = sc.nextInt();
+        	y1 = sc.nextInt();
+        	x2 = sc.nextInt();
+        	y2 = sc.nextInt();
+    
+        	int lengthOfLine = (int)Math.sqrt((x1-x2)^2 -(y1-y2)^2);
+        
+        return lengthOfLine;
+    }
+	public static void main (String[] args) 
+	{
+        int lengthOfLine1 = lengthOfLine();
+        
+		System.out.println("Length of Line 1 :" + lengthOfLine1);
 
-	        int length_of_line_1 = (int)Math.sqrt((x1-x2)^2 -(y1-y2)^2);
-	        System.out.println("Length Of line 1 :"+length_of_line_1);
+        int lengthOfLine2 = lengthOfLine();
+		System.out.println("Length of Line 2 :" + lengthOfLine2);
 
+        Integer lenOfLine1 = lengthOfLine1;
+        Integer lenOfLine2 = lengthOfLine2;
 
-	        int x3,y3,x4,y4;
-	        Scanner sc1 = new Scanner(System.in);
-	        x3 = sc1.nextInt();
-	        y3 = sc1.nextInt();
-	        x4 = sc1.nextInt();
-	        y4 = sc1.nextInt();
-
-	        int length_of_line_2 = (int)Math.sqrt((x3-x4)^2 -(y3-y4)^2);
-
-	        System.out.println("Length Of line 2 :"+length_of_line_2);
-
-	        
-	        Integer len_of_line1 = length_of_line_1;
-	        Integer len_of_line2 = length_of_line_2;
-	        
-	       int value =  len_of_line1.compareTo(len_of_line2);
-	       System.out.println(value);
-
-	        if(value==0)
-	            System.out.println("Two lines are equal");
-	        else if(value > 0)
-	            System.out.println("Line 1 is greater than Line 2");
-	        else
-	            System.out.println("Line 1 is less than Line 2");
-	}
+        if(lenOfLine1.equals(lenOfLine2))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
 
 }
