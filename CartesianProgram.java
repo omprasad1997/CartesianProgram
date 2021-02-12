8a28435516adf9e08093a1bf1a463c9424486ee7
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-12 21:02:58 +0530

    Changed Spaces

--- CartesianProgram.java
+++ CartesianProgram.java
@@ -17,25 +17,25 @@ public class CartesianProgram {
     }
 	public static void main (String[] args) {
 		
-		int lengthOfLine1 = lengthOfLine();
+			int lengthOfLine1 = lengthOfLine();
         
-		System.out.println("Length of Line 1 :" + lengthOfLine1);
+			System.out.println("Length of Line 1 :" + lengthOfLine1);
 
-        int lengthOfLine2 = lengthOfLine();
-		System.out.println("Length of Line 2 :" + lengthOfLine2);
+			int lengthOfLine2 = lengthOfLine();
+			System.out.println("Length of Line 2 :" + lengthOfLine2);
 
-        Integer lenOfLine1 = lengthOfLine1;
-        Integer lenOfLine2 = lengthOfLine2;
+			Integer lenOfLine1 = lengthOfLine1;
+			Integer lenOfLine2 = lengthOfLine2;
 
-        int value =  lenOfLine1.compareTo(lenOfLine2);
-        System.out.println("Value :"+ value);
+			int value =  lenOfLine1.compareTo(lenOfLine2);
+			System.out.println("Value :"+ value);
 
-        if(value==0)
-            System.out.println("Two lines are equal");
-        else if(value > 0)
-            System.out.println("Line 1 is greater than Line 2");
-        else
-            System.out.println("Line 1 is less than Line 2");
+			if(value==0)
+				System.out.println("Two lines are equal");
+			else if(value > 0)
+				System.out.println("Line 1 is greater than Line 2");
+			else
+				System.out.println("Line 1 is less than Line 2");
     }
 
 }
