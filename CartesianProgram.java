a0b626f33cdaf1d80f2c31b918f20aaa88766cb9
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-12 20:43:53 +0530

    Use Naming Convention

--- CartesianProgram.java
+++ CartesianProgram.java
@@ -4,21 +4,20 @@ public class CartesianProgram {
 
 	public static int lengthOfLine()
     {
-        	int x1,y1,x2,y2;
-        	Scanner sc = new Scanner(System.in);
-        	x1 = sc.nextInt();
-        	y1 = sc.nextInt();
-        	x2 = sc.nextInt();
-        	y2 = sc.nextInt();
+        int x1,y1,x2,y2;
+        Scanner sc = new Scanner(System.in);
+        x1 = sc.nextInt();
+        y1 = sc.nextInt();
+        x2 = sc.nextInt();
+        y2 = sc.nextInt();
     
-        	int lengthOfLine = (int)Math.sqrt((x1-x2)^2 -(y1-y2)^2);
+        int lengthOfLine = (int)Math.sqrt((x1-x2)^2 -(y1-y2)^2);
         
         return lengthOfLine;
     }
 	public static void main (String[] args) 
 	{
         int lengthOfLine1 = lengthOfLine();
-        
 		System.out.println("Length of Line 1 :" + lengthOfLine1);
 
         int lengthOfLine2 = lengthOfLine();
