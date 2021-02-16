a34d5471bb0573a3b1484e731b3989ee18d12f8a
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-16 13:59:56 +0530

    Using OOPS

--- CartesianProgram.java
+++ CartesianProgram.java
@@ -2,40 +2,52 @@ package com.blbz;
 import java.util.*;
 public class CartesianProgram {
 
-	public static int lengthOfLine()
-    {
-        int x1,y1,x2,y2;
-        Scanner sc = new Scanner(System.in);
-        x1 = sc.nextInt();
-        y1 = sc.nextInt();
-        x2 = sc.nextInt();
-        y2 = sc.nextInt();
-    
-        int lengthOfLine = (int)Math.sqrt((x1-x2)^2 -(y1-y2)^2);
-        
-        return lengthOfLine;
-    }
 	public static void main (String[] args) {
 		
-			int lengthOfLine1 = lengthOfLine();
+		LineComparison line1 = new LineComparison();
+        line1.lengthCalculate(6,1,6,2);
+        System.out.println("Length Of Line 1 : " + line1.lengthOfLine);
+
+        LineComparison line2 = new LineComparison();
+        line2.lengthCalculate(7,4,3,3);
+        System.out.println("Length Of Line 2 : " + line2.lengthOfLine);
+
+        Integer len1 = line1.lengthOfLine;
+        Integer len2 = line2.lengthOfLine;
+
+        LineComparison equal = new LineComparison();
+        System.out.println("Check whether two lines are equal : " + equal.checkEquality(len1,len2));
         
-			System.out.println("Length of Line 1 :" + lengthOfLine1);
+        LineComparison compare = new LineComparison();
+        System.out.println("Result of compare : " + compare.checkComapre(len1,len2));
 
-			int lengthOfLine2 = lengthOfLine();
-			System.out.println("Length of Line 2 :" + lengthOfLine2);
+        if(compare.checkComapre(len1,len2)==0)
+            System.out.println("Two lines are equal");
+        else if(compare.checkComapre(len1,len2) > 0)
+            System.out.println("Line 1 is greater than Line 2");
+        else
+            System.out.println("Line 1 is less than Line 2");
+    }
+}
 
-			Integer lenOfLine1 = lengthOfLine1;
-			Integer lenOfLine2 = lengthOfLine2;
+class LineComparison{
+    int x1,y1,x2,y2;
+    Integer lengthOfLine;
 
-			int value =  lenOfLine1.compareTo(lenOfLine2);
-			System.out.println("Value :"+ value);
-
-			if(value==0)
-				System.out.println("Two lines are equal");
-			else if(value > 0)
-				System.out.println("Line 1 is greater than Line 2");
-			else
-				System.out.println("Line 1 is less than Line 2");
+    void lengthCalculate(int a1,int b1,int a2,int b2){
+        lengthOfLine = (int)Math.sqrt((a1-a2)^2 -(b1-b2)^2);
     }
 
+    boolean checkEquality(Integer len1,Integer len2)
+    {
+        if(len1.equals(len2))
+            return true;
+        else    
+           return  false;
+    }
+    public int checkComapre(Integer first , Integer second)
+    {
+        int value = first.compareTo(second);
+        return value;
+    }
 }
